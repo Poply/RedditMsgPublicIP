c3b355950e55e32f86ef004b9fb8d1aa01b63652
poply <poplyx102@hotmail.com>
2016-11-01 08:39:55 -0700

    project files

---
+++ GetPublicIP.py
@@ -0,0 +1,26 @@
+import urllib.request
+import praw
+import sys
+
+def reddit_message(myIP):
+    r = praw.Reddit("My Assistant")
+    r.login("MYACCOUNT", "MYPASSWORD")                  #Custom setting
+    r.send_message("MYOTHERACCOUNT", "new ip", myIP)    #Custom setting
+
+fileLocation = "/home/pi/scripts/currentip.txt"
+f = open(fileLocation, 'r+')
+oldIP = f.read()[:-1] #TODO currently cutting off last character because it is picking up a garbage char and ruining the if...then check when comparing IPs
+
+try:
+    myIP = (urllib.request.urlopen('http://ip.42.pl/raw').read()).decode()
+except (urllib.error.HTTPError, urllib.error.URLError) as err:
+    print(err)
+    sys.exit()
+else:
+    if (myIP != oldIP):
+        f.seek(0)
+        f.write(myIP)
+        reddit_message(myIP)
+        print("not equal, sending message ", len(myIP), len(oldIP), (myIP == oldIP))
+finally:
+    f.close()
